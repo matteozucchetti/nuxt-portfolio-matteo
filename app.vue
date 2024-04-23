0c98f3c6fed3bf6d067d6850faa8795331fe6147
Matteo Zucchetti <zucchetti.matteo@gmail.com>
2024-04-23 14:28:21 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div ref="loader" class="h-full w-full fixed bg-black z-20" />
+    <NuxtPage class="h-dvh" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import gsap from "gsap";
+
+const nuxtApp = useNuxtApp();
+const loader = ref(null);
+nuxtApp.hook("page:finish", () => {
+  setTimeout(() => {
+    gsap.to(loader.value, {
+      duration: 1,
+      opacity: 0,
+      pointerEvents: "none",
+    });
+  }, 300);
+});
+
+useSeoMeta({
+  title: "Matteo Zucchetti | Portfolio",
+  ogTitle: "Matteo Zucchetti | Portfolio",
+  description: "Front-end Developer.",
+  ogDescription: "Front-end Developer.",
+});
+
+useHead({
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.ico",
+    },
+  ],
+});
+</script>
